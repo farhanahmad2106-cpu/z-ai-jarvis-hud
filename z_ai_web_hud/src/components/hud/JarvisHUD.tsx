78f3c3a1d592bbd714a33b56ea209ce66a7e4b00
Farhan-Ahmad3666 <farhanahmad2106@gmail.com>
2026-09-13 21:58:04 +0530

    Make left sidebar scrollbar visible with custom cyan styling

--- z_ai_web_hud/src/components/hud/JarvisHUD.tsx
+++ z_ai_web_hud/src/components/hud/JarvisHUD.tsx
@@ -407,7 +407,7 @@ export const JarvisHUD: React.FC = () => {
       </header>
 
       {/* Left Wing: Altimeter & Telemetry */}
-      <aside className="absolute left-8 top-1/2 -translate-y-1/2 flex flex-col gap-6 z-40 max-h-[55vh] overflow-y-auto scrollbar-hide pr-2 pb-4">
+      <aside className="absolute left-8 top-1/2 -translate-y-1/2 flex flex-col gap-6 z-40 max-h-[55vh] overflow-y-auto pr-2 pb-4 [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:bg-cyan/30 [&::-webkit-scrollbar-track]:bg-transparent hover:[&::-webkit-scrollbar-thumb]:bg-cyan/50">
         <div className="relative">
           <div className={`flex flex-col gap-6 transition-all duration-500 ${modules.telemetry ? '' : 'opacity-20 blur-sm pointer-events-none'}`}>
             <TodoListWidget />
